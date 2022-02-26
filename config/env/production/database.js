aeb5e9d7dd1404bcef270a8971bb9c50f45e448e
djkenneth <dj.kenneth.pineda@gmail.com>
2022-02-26 16:34:14 +0800

    Added url in database file

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,6 +1,8 @@
 const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
+  url: env("https://strapi-cms-api-app.herokuapp.com"),
+  proxy: true,
   connection: {
     client: "postgres",
     connection: {
