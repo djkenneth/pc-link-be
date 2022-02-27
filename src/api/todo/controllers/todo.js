2ad4eacffdf4b7f92ca1f144f9829d18cbd4c344
djkenneth <dj.kenneth.pineda@gmail.com>
2022-02-27 16:52:26 +0800

    update the collection

--- src/api/todo/controllers/todo.js
+++ src/api/todo/controllers/todo.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  book controller
+ *  todo controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::book.book');
+module.exports = createCoreController('api::todo.todo');
