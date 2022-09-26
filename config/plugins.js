6548e8495e534ad1237adc8eab9be016faed1494
djkenneth <dj.kenneth.pineda@gmail.com>
2022-09-26 12:43:36 +0800

    new update

--- config/plugins.js
+++ config/plugins.js
@@ -17,4 +17,16 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
 });
