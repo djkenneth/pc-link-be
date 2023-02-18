be46508f1b173376ff4eb1fea6ade65e533a941c
djkenneth <dj.kenneth.pineda@gmail.com>
2023-02-18 19:36:31 +0800

    Added Categories

---
+++ src/api/sub-child-category/controllers/sub-child-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sub-child-category controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::sub-child-category.sub-child-category');
